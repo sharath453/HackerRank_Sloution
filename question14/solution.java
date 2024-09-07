1fcddabf0d87be17d511f19d4cd894e8d5f5b746
sharath <alsharath66@gmail.com>
2024-09-07 19:26:47 +0530

    problem14

---
+++ question14/solution.java
@@ -0,0 +1,56 @@
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'kangaroo' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER x1
+     *  2. INTEGER v1
+     *  3. INTEGER x2
+     *  4. INTEGER v2
+     */
+
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        // If one kangaroo starts ahead and also jumps farther per move, they will never meet.
+        if (v1 == v2) {
+            if (x1 == x2) {
+                return "YES"; // Same starting point and same velocity
+            } else {
+                return "NO"; // Same velocity but different starting points
+            }
+        } else {
+            // If they are moving towards each other, and the relative distance can be divisible by relative velocity
+            if ((x2 - x1) % (v1 - v2) == 0 && (x1 < x2 && v1 > v2 || x2 < x1 && v2 > v1)) {
+                return "YES";
+            } else {
+                return "NO";
+            }
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int x1 = Integer.parseInt(firstMultipleInput[0]);
+        int v1 = Integer.parseInt(firstMultipleInput[1]);
+        int x2 = Integer.parseInt(firstMultipleInput[2]);
+        int v2 = Integer.parseInt(firstMultipleInput[3]);
+
+        String result = Result.kangaroo(x1, v1, x2, v2);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
