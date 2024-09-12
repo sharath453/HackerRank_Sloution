6c7db79bea8703d4c712b496b6c2fac99c62fe8b
sharath <alsharath66@gmail.com>
2024-09-12 19:40:33 +0530

    question19

---
+++ question19/solution.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+class Result {
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+    public static int migratoryBirds(List<Integer> arr) {
+        
+        int[] frequencies = new int[6]; 
+
+        for (int bird : arr) {
+            frequencies[bird]++;
+        }
+
+        int maxFrequency = 0;
+        int birdTypeWithMaxFrequency = Integer.MAX_VALUE;
+
+        for (int birdType = 1; birdType <= 5; birdType++) {
+            if (frequencies[birdType] > maxFrequency) {
+                maxFrequency = frequencies[birdType];
+                birdTypeWithMaxFrequency = birdType;
+            } else if (frequencies[birdType] == maxFrequency && birdType < birdTypeWithMaxFrequency) {
+                birdTypeWithMaxFrequency = birdType;
+            }
+        }
+
+        return birdTypeWithMaxFrequency;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < arrCount; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        int result = Result.migratoryBirds(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
