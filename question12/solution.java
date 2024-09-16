88045e62071213f41fa7783edee10402dfcba12d
sharath <alsharath66@gmail.com>
2024-09-16 23:25:28 +0530

    question12 edit

--- question12/solution.java
+++ question12/solution.java
@@ -8,13 +8,6 @@ import java.util.regex.*;
 
 class Result {
 
-    /*
-     * Complete the 'gradingStudents' function below.
-     *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts INTEGER_ARRAY grades as parameter.
-     */
-
     public static List<Integer> gradingStudents(List<Integer> grades) {
     // Write your code here
     int n=grades.size();
