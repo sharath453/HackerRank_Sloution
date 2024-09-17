5357ae533c2be6b7176e1f6283add50fc0e34fb4
sharath <alsharath66@gmail.com>
2024-09-17 18:50:13 +0530

    question22

--- question22/solution.java
+++ question22/solution.java
@@ -11,6 +11,7 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Result {
+    
     public static List<Integer> rotateLeft(int d, List<Integer> arr)
     {
         int n=arr.size();
