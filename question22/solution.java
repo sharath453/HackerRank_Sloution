6f72b438b5f81c596293ad1957639448cda4e148
sharath <alsharath66@gmail.com>
2024-09-18 23:30:16 +0530

    question22

--- question22/solution.java
+++ question22/solution.java
@@ -41,6 +41,7 @@ class Result {
     }
 
 }
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
