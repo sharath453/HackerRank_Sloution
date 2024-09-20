30763e95359f52baef4dedc5f7f0a789f0664255
sharath <alsharath66@gmail.com>
2024-09-20 16:44:20 +0530

    question27

---
+++ question27/solution.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); // Number of test cases
+
+        for (int i = 0; i < t; i++) {
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+
+                // Check if the value fits in a byte
+                if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE) {
+                    System.out.println("* byte");
+                }
+                // Check if the value fits in a short
+                if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE) {
+                    System.out.println("* short");
+                }
+                // Check if the value fits in an int
+                if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE) {
+                    System.out.println("* int");
+                }
+                // Check if the value fits in a long
+                if (x >= Long.MIN_VALUE && x <= Long.MAX_VALUE) {
+                    System.out.println("* long");
+                }
+            } catch (Exception e) {
+                // If the number is too large to be read as a long, print the error message
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+        }
+    }
+}
