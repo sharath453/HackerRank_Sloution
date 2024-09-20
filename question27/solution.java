90f01ac82953daef01440cf7fa976433f4a235ed
sharath <alsharath66@gmail.com>
2024-09-20 16:46:22 +0530

    question27

--- question27/solution.java
+++ question27/solution.java
@@ -4,7 +4,7 @@ import java.io.*;
 class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt(); // Number of test cases
+        int t = sc.nextInt(); 
 
         for (int i = 0; i < t; i++) {
             try {
