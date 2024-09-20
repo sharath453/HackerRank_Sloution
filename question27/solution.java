8e34310f1bff7f939e4684555d067bdcb1bc846d
sharath <alsharath66@gmail.com>
2024-09-20 16:45:55 +0530

    question27

--- question27/solution.java
+++ question27/solution.java
@@ -15,7 +15,7 @@ class Solution {
                 if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE) {
                     System.out.println("* byte");
                 }
-                // Check if the value fits in a short
+
                 if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE) {
                     System.out.println("* short");
                 }
@@ -23,7 +23,7 @@ class Solution {
                 if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE) {
                     System.out.println("* int");
                 }
-                
+
                 if (x >= Long.MIN_VALUE && x <= Long.MAX_VALUE) {
                     System.out.println("* long");
                 }
