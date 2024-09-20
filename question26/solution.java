fadfadc83a75eca42737898c77eb01c5c472b23b
sharath <alsharath66@gmail.com>
2024-09-20 16:40:44 +0530

    question26

--- question26/solution.java
+++ question26/solution.java
@@ -9,7 +9,6 @@ public class Solution {
             {
                 String s1=sc.next();
                 int x=sc.nextInt();
-               
                 System.out.printf("%-15s%03d%n", s1, x);
             }
             System.out.println("================================");
