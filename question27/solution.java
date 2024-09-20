16097eaaf8de55184eb4a47c02d766be5a822484
sharath <alsharath66@gmail.com>
2024-09-20 16:52:18 +0530

    question27

--- question27/solution.java
+++ question27/solution.java
@@ -2,7 +2,8 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt(); 
 
