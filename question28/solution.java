47a93e14bff1d8cb1a36a9a01ee25ea5afc51f82
sharath <alsharath66@gmail.com>
2024-09-21 21:53:15 +0530

    question28

---
+++ question28/solution.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    private Stack<Character> stacke=new Stack<>();
+    private Queue<Character> newqueue=new LinkedList<>();
+    
+    void pushCharacter(char c){
+     stacke.push(c);   
+    }
+    
+    void enqueueCharacter(char ch){
+        newqueue.add(ch);
+    }
+    
+    public char popCharacter()
+    {
+        return stacke.pop();
+    }
+    
+     public char dequeueCharacter()
+    {
+        return newqueue.remove();
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        scan.close();
+
+        // Convert input String to an array of characters:
+        char[] s = input.toCharArray();
+
+        // Create a Solution object:
+        Solution p = new Solution();
+
+        // Enqueue/Push all chars to their respective data structures:
+        for (char c : s) {
+            p.pushCharacter(c);
+            p.enqueueCharacter(c);
+        }
+
+        // Pop/Dequeue the chars at the head of both data structures and compare them:
+        boolean isPalindrome = true;
+        for (int i = 0; i < s.length/2; i++) {
+            if (p.popCharacter() != p.dequeueCharacter()) {
+                isPalindrome = false;                
+                break;
+            }
+        }
+
+        //Finally, print whether string s is palindrome or not.
+        System.out.println( "The word, " + input + ", is " 
+                           + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
+    }
+}
