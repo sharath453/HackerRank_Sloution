fbb7fd89a124976dc0469b88460b6d855cdcbae9
sharath <alsharath66@gmail.com>
2024-09-22 21:23:24 +0530

    question29

--- question29/solution.java
+++ question29/solution.java
@@ -4,7 +4,7 @@ import java.io.*;
 class Solution {
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
-        int t = in.nextInt();  // Number of queries
+        int t = in.nextInt();
     
         for(int i = 0; i < t; i++){
             int a = in.nextInt();  
