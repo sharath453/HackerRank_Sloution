d1312165c80297ed88faa94894c3792d21786b1d
sharath <alsharath66@gmail.com>
2024-09-26 21:55:27 +0530

    question32

--- question32/solution.java
+++ question32/solution.java
@@ -10,7 +10,6 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
