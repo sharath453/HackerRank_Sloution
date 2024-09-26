3cf0ebfff64792310e4cd6d5c403fdfa29637cc1
sharath <alsharath66@gmail.com>
2024-09-26 21:51:40 +0530

    question32

--- question32/solution.java
+++ question32/solution.java
@@ -31,8 +31,7 @@ public class Solution {
                 a.set(j + 1, temp);
             numberOfSwaps++;
         }
-    }
-    
+    }   
     // If no elements were swapped during a traversal, array is sorted
     if (numberOfSwaps == 0) {
         break;
